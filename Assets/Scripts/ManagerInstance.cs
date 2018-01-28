b754aa1ac453b48e24ac0cc20035de6c4cdb4556
Caleb McKinney <calebamckinney@gmail.com>
2018-01-28 16:01:06 -0600

    Object pooling tests

---
+++ Assets/Scripts/ManagerInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerInstance : MonoBehaviour {
+    public GameObject prefab;
+
+    void Start()
+    {
+        PoolManager.instance.CreatePool(prefab, 3);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PoolManager.instance.ReuseObject(prefab, Vector3.one, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/TestManger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManger : MonoBehaviour {
+	public GameObject prefab;
+	
+	// Use this for initialization
+	void Start () {
+		PoolManager.instance.CreatePool(prefab, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			PoolManager.instance.ReuseObject(prefab, Vector3.one, Quaternion.identity);
+		}
+	}
+}
